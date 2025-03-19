26097142a2fce26eaabc0dd8ebe93c1cf535483f
pranjulchaturvedi007 <pranjulchaturvedi561@gmail.com>
2025-03-19 20:19:39 +0530

    Here is test folder.

---
+++ RevisionTest/pyramid.java
@@ -0,0 +1,11 @@
+package RevisionTest;
+
+public class pyramid {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("  * * * *");
+        System.out.println("    * * *");
+        System.out.println("      * *");
+        System.out.println("        *");}
+    
+}
